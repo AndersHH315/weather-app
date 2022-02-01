42fb453ff76d6afa110096f770a74da38d6fd76d
Jonas Nygren <jonas_nygren30@hotamil.com>
2022-02-01 11:22:33 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather defaultCity="Amsterdam" />
+        <Weather defaultCity="Örebro" />
       </header>
     </div>
   );
